9dbb14fbc0817ebc6490f046f6b476bac7778a22
Agl <agl@localhost.(none)>
2010-09-06 18:20:30 +0200

    License stuff

---
+++ PimpedTableTest.scala
@@ -0,0 +1,17 @@
+/* PimpedScalaTable is a wrapper arround scalas swing.table or Javas JTable (haven't decided jet ;) ) that is meant to evercome all shortcomings I find lacking in scalas swing.table 
+
+
+Copyright (C) 2010 Axel Gschaider
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. 
+
+
+If you would like to obtain this programm under another license, feel free to contact me: 
+axel[dot]gschaider[at]gmx[dot]at
+or http://github.com/axelGschaider
+*/
+
